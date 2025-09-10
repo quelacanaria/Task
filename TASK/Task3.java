09eb54ef03219e758cd8110ba436ea8987adc83b
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2025-09-11 00:37:05 +0800

    Task3.java

--- TASK/Task3.java
+++ TASK/Task3.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package TASK;
 
 public class Task3 {
@@ -17,23 +16,3 @@ public class Task3 {
         }
     }
 }
-=======
-package TASK;
-
-public class Task3 {
-    public static void main(String[] args) {
-        String a = "Wow";
-        String b = "Wow";
-        String c = "!Wow";
-        String d = "Wow!"; 
-
-        boolean b1 = a==b;
-        boolean b2 = d.equals(b+"!");
-        boolean b3 = !c.equals(a);
-
-        if(b1&&b2&&b3){   
-            System.out.println("Success!");
-        }
-    }
-}
->>>>>>> a02cd7bc28b031a908f2aeca9c905e873f043451
