74bb4e2814981a443d37cc6bad7c31a4daa6091b
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2025-09-11 00:38:42 +0800

    commit

--- TASK/Task5.java
+++ TASK/Task5.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package TASK;
 import java.util.*;
 public class Task5 {
@@ -16,24 +15,3 @@ public class Task5 {
         }
     }
 }
-
-=======
-package TASK;
-import java.util.*;
-public class Task5 {
-    public static void main(String[] args) {
-        System.out.println("Enter 3 numbers: ");
-        Scanner input = new Scanner(System.in);
-        int[] numbers = new int[3];
-
-        for(int x=0;x<numbers.length;x++){
-            numbers[x] = input.nextInt(); 
-        }
-        Arrays.sort(numbers);
-        for(int number: numbers){  
-            System.out.print(number+" , ");
-        }
-    }
-}
-
->>>>>>> a02cd7bc28b031a908f2aeca9c905e873f043451
