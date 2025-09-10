515ed903c9fd740114fad8241555ca3836bddaf3
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2025-09-11 00:40:45 +0800

    commit

--- TASK/tasksheet_1_1_1.java
+++ TASK/tasksheet_1_1_1.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package TASK;
 
 public class tasksheet_1_1_1 {
@@ -8,15 +7,3 @@ class helloWorldApp{
     public static void main(String[] args) {
         System.out.println("Kamusta Mundo!"); // Display the string.
     }
-=======
-package TASK;
-
-public class tasksheet_1_1_1 {
-   
-}
-class helloWorldApp{
-    public static void main(String[] args) {
-        System.out.println("Kamusta Mundo!"); // Display the string.
-    }
->>>>>>> a02cd7bc28b031a908f2aeca9c905e873f043451
-    }  
