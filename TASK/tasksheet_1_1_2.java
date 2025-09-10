073c005791ae34f10ecadf7fd9cc4a92e8b44c7c
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2025-09-11 00:41:11 +0800

    commit

--- TASK/tasksheet_1_1_2.java
+++ TASK/tasksheet_1_1_2.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package TASK;
 
 public class tasksheet_1_1_2 {
@@ -26,33 +25,3 @@ class ArithmeticTask {
         System.out.println(result);
   
     }
-=======
-package TASK;
-
-public class tasksheet_1_1_2 {
-    
-}
-class ArithmeticTask {
-
-    public static void main (String[] args){
-          
-        int result = 1 + 2; // result is now 3
-        System.out.println(result);
-
-        result = result - 1; // result is now 2
-        System.out.println(result);
-
-        result = result * 2; // result is now 4
-        System.out.println(result);
-
-        result = result / 2; // result is now 2
-        System.out.println(result);
-
-        result = result + 8; // result is now 10
-        System.out.println(result);
-        result = result % 7; // result is now 3
-        System.out.println(result);
-  
-    }
->>>>>>> a02cd7bc28b031a908f2aeca9c905e873f043451
-}
