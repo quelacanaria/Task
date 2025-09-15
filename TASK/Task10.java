baa5f5829adc4e5d983900e8d04e157ca11b0fd5
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2025-09-15 16:20:19 +0800

    commit

---
+++ TASK/Task10.java
@@ -0,0 +1,30 @@
+package TASK;
+
+public class Task10 {
+     class Student {
+        private String firstName;
+        private String lastName;
+        public Student(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+        // Your code goes here
+        public void printFullName(){
+            System.out.println(firstName + " " + lastName);
+        }
+    }
+
+    public class Tasksheet10  {
+        public static void main(String[] args) {
+            Student[] students = new Student[] {
+                    new Student("Morgan", "Freeman"),
+                    new Student("Brad", "Pitt"),
+                    new Student("Kevin", "Spacey"),
+            };
+            for (Student s : students) {
+                s.printFullName();
+            }
+        }
+    }
+
+}
